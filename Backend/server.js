7cc497d8da5787e74677b8aef71c25417399f7cc
Niharika <niharikamalankar@gmail.com>
2026-07-09 06:14:50 +0530

    Updated server for Render deployment

--- Backend/server.js
+++ Backend/server.js
@@ -72,8 +72,8 @@ app.post("/send-bill-email", async(req,res)=>{
 
 
 
-app.listen(5000,()=>{
+const PORT = process.env.PORT || 5000;
 
-console.log("Backend running on 5000");
-
+app.listen(PORT, () => {
+  console.log(`Backend running on ${PORT}`);
 });
